8de980ccc3c0de481b51e137ebe28920ba0dc225
Adam <adam.przybyla.prv@gmail.com>
2025-04-19 15:18:22 +0200

    1902_1518
    Ranking and modals

--- Scripts.js
+++ Scripts.js
@@ -6,3 +6,10 @@ function openTab(evt, tabId) {
     evt.currentTarget.classList.add('active');
     document.getElementById(tabId).classList.add('active');
   }
+
+  window.addEventListener('message', function(event) {
+    if (event.origin === 'http://twoja-domena.com') { // Sprawdź, czy wiadomość pochodzi z właściwej domeny
+      const iframe = document.getElementById('mojeIframe');
+      iframe.style.height = event.data + 'px'; // Ustawiamy wysokość iframe na wartość z wiadomości
+    }
+  });
---
+++ ScriptsRanking.js
@@ -0,0 +1,14 @@
+function showModal(button) {
+    document.getElementById('modalText').textContent = `Random Text`;
+    document.getElementById('myModal').style.display = 'flex';
+  }
+  
+  function closeModal() {
+    document.getElementById('myModal').style.display = 'none';
+  }
+
+
+  window.onload = function() {
+    const height = document.body.scrollHeight;
+    parent.postMessage(height, '*'); // Wysyłamy wysokość do strony nadrzędnej
+  };
